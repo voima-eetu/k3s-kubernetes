k3s-io/k3s

kubelet/cm: ignore sysctl error when running in userns
Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

pkg/kubelet/cm/container_manager_linux.go
@@ -32,6 +32,7 @@ import (
 	"github.com/opencontainers/runc/libcontainer/cgroups"
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs"
 	"github.com/opencontainers/runc/libcontainer/configs"
+	libcontainersystem "github.com/opencontainers/runc/libcontainer/system"
 	"k8s.io/klog"
 
 	v1 "k8s.io/api/core/v1"
@@ -399,7 +400,11 @@ func setupKernelTunables(option KernelTunableBehavior) error {
 			klog.V(2).Infof("Updating kernel flag: %v, expected value: %v, actual value: %v", flag, expectedValue, val)
 			err = sysctl.SetSysctl(flag, expectedValue)
 			if err != nil {
-				errList = append(errList, err)
+				if libcontainersystem.RunningInUserNS() {
+					klog.Warningf("Updating kernel flag failed: %v: %v (running in UserNS)", flag, err)
+				} else {
+					errList = append(errList, err)
+				}
 			}
 		}
 	}

voima-eetu/k3s-kubernetes

pkg/kubelet/cm/container_manager_linux.go
@@ -33,6 +33,7 @@ import (
 	cgroupfs "github.com/opencontainers/runc/libcontainer/cgroups/fs"
 	cgroupfs2 "github.com/opencontainers/runc/libcontainer/cgroups/fs2"
 	"github.com/opencontainers/runc/libcontainer/configs"
+	libcontaineruserns "github.com/opencontainers/runc/libcontainer/userns"
 	"k8s.io/klog/v2"
 	"k8s.io/mount-utils"
 	utilio "k8s.io/utils/io"
@@ -455,7 +456,11 @@ func setupKernelTunables(option KernelTunableBehavior) error {
 			klog.V(2).InfoS("Updating kernel flag", "flag", flag, "expectedValue", expectedValue, "actualValue", val)
 			err = sysctl.SetSysctl(flag, expectedValue)
 			if err != nil {
-				errList = append(errList, err)
+				if libcontaineruserns.RunningInUserNS() {
+					klog.Warningf("Updating kernel flag failed: %v: %v (running in UserNS)", flag, err)
+				} else {
+					errList = append(errList, err)
+				}
 			}
 		}
 	}