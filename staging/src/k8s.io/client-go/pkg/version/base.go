be0d99312ad046db478d69d36164d103a96d6f56
korvoj <korvoj@users.noreply.github.com>
2023-01-26 08:19:39 +0000

    Revert "modify base.go"
    This reverts commit f5043f3f8488cd9d494351427c6f9cbc79ca3920.

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -1,11 +1,10 @@
 package version
 
 var (
-	gitMajor             = "1"
-	gitMinor             = "22"
-	gitVersion   = "v1.22.17-custom-changes-01"
-	gitCommit    = "8e518a5808934a30bbd800a5fa8f004afa87d45f"
+	gitMajor = "1"
+	gitMinor = "22"
+	gitVersion   = "v1.22.17-k3s1"
+	gitCommit    = "e48441a0b916e9c9152f019e9a5a39a51b72bb88"
 	gitTreeState = "clean"
-	buildDate          = "2023-01-26T08:16:17Z"
+	buildDate = "2022-12-09T01:47:16Z"
 )
-
