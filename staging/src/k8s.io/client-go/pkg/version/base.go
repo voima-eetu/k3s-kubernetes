7acc0041e2ac2e1b6ed052630add1b63c7884c90
korvoj <korvoj@users.noreply.github.com>
2023-01-26 08:35:15 +0000

    v1.22.17-custom-changes

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "22"
-	gitVersion   = "v1.22.17-k3s1"
-	gitCommit    = "e48441a0b916e9c9152f019e9a5a39a51b72bb88"
+	gitVersion   = "v1.22.17-custom-changes"
+	gitCommit    = "be0d99312ad046db478d69d36164d103a96d6f56"
 	gitTreeState = "clean"
-	buildDate = "2022-12-09T01:47:16Z"
+	buildDate = "2023-01-26T08:28:30Z"
 )
