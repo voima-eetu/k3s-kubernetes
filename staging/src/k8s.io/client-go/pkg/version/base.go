d9f927c8785062a67d814239272c412e98c68ca3
korvoj <korvoj@users.noreply.github.com>
2023-01-29 13:08:41 +0000

    v1.22.17-log06

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "22"
-	gitVersion   = "v1.22.17-log05"
-	gitCommit    = "4c24af0b9d948fcfe5e478f0c5346498990bdd65"
+	gitVersion   = "v1.22.17-log06"
+	gitCommit    = "6903840a53104335033145414077446d8060fc96"
 	gitTreeState = "clean"
-	buildDate = "2023-01-29T10:24:05Z"
+	buildDate = "2023-01-29T13:03:19Z"
 )
