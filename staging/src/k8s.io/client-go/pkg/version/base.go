07431aaf0bfbb50b84b5b11cb03c80957eab9251
eetu <eetu@voimagold.com>
2023-07-31 16:54:34 +0300

    v1.27.4-log07

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "27"
-	gitVersion   = "v1.27.4-k3s1"
-	gitCommit    = "ff335b768859f0f8ffe17215f62ef3252f92859d"
+	gitVersion   = "v1.27.4-log07"
+	gitCommit    = "40562dc3ce7d276655a4993f95f4d6ce1fc4683b"
 	gitTreeState = "clean"
-	buildDate = "2023-07-19T23:58:43Z"
+	buildDate = "2023-07-31T13:51:26Z"
 )
